70862bc5f42d853b17e39341ee2adbdcaafaf058
mrdotx <klassiker@gmx.de>
2020-02-09 14:29:04 +0100

    changed to bc for hex to dev conversion

--- undervoltage.sh
+++ undervoltage.sh
@@ -3,7 +3,7 @@
 # path:       ~/projects/shell/raspberrypi/undervoltage.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2020-02-03T13:24:19+0100
+# date:       2020-02-09T14:25:45+0100
 
 # information for results
 # 0: under-voltage
@@ -38,16 +38,17 @@
 
 vcgencmd="/opt/vc/bin/vcgencmd"
 cpu_freq="/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq"
-echo "to stop simply press [ctrl]-[c]"
+echo "to stop press [ctrl]-[c]"
 i=14
-header="time       temp  cpu fake/real     health state    vcore"
+header="time      temp   cpu fake/real     health state    vcore"
 while true; do
     i=$(( i + 1 ))
     if [ "$i" -eq 15 ]; then
         echo "$header"
         i=0
     fi
-    health=$(perl -e "printf \"%19b\n\", $($vcgencmd get_throttled | cut -f2 -d=)")
+    throttled=$($vcgencmd get_throttled | cut -f2 -dx)
+    health=$(echo "obase=2; ibase=16; $throttled" | bc)
     temp=$($vcgencmd measure_temp | cut -f2 -d=)
     real_cs=$($vcgencmd measure_clock arm | awk -F"=" '{printf ("%0.0f",$2/1000000); }')
     sys_cs=$(awk '{printf ("%0.0f",$1/1000); }' <$cpu_freq)
