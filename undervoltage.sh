a9326f4c15e12de672a51bca1037f54ab4b5267e
mrdotx <klassiker@gmx.de>
2020-02-10 21:19:05 +0100

    small fixes

--- undervoltage.sh
+++ undervoltage.sh
@@ -3,7 +3,7 @@
 # path:       ~/projects/shell/raspberrypi/undervoltage.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2020-02-09T14:25:45+0100
+# date:       2020-02-10T21:16:40+0100
 
 # information for results
 # 0: under-voltage
@@ -15,7 +15,7 @@
 # 18: throttling has occurred
 
 # bad values
-#     Health state    Vcore
+# health state        vcore
 # 1000000000000000000 1.3125V
 # 1010000000000000000 1.3125V
 # 1010000000000000101 1.2V
@@ -26,7 +26,7 @@
 # 1010000000000000000 1.3125V
 
 # good values
-#     Health state    Vcore
+# health state        vcore
 # 0000000000000000000 1.3125V
 # 0000000000000000000 1.3125V
 # 0000000000000000000 1.3125V
@@ -40,7 +40,7 @@ vcgencmd="/opt/vc/bin/vcgencmd"
 cpu_freq="/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq"
 echo "to stop press [ctrl]-[c]"
 i=14
-header="time      temp   cpu fake/real     health state    vcore"
+header="time     temp    cpu fake/real  health state        vcore"
 while true; do
     i=$(( i + 1 ))
     if [ "$i" -eq 15 ]; then
@@ -53,6 +53,6 @@ while true; do
     real_cs=$($vcgencmd measure_clock arm | awk -F"=" '{printf ("%0.0f",$2/1000000); }')
     sys_cs=$(awk '{printf ("%0.0f",$1/1000); }' <$cpu_freq)
     v_core=$($vcgencmd measure_volts | cut -f2 -d= | sed 's/000//')
-    echo "$(date "+%H:%M:%S") $temp$(printf "%5s" "$sys_cs")/$(printf "%4s" "$real_cs") MHz $(printf "%019d" "$health") $v_core"
+    echo "$(date "+%H:%M:%S") $temp$(printf "%5s" "$sys_cs")/$(printf "%4s" "$real_cs") MHz   $(printf "%019d" "$health") $v_core"
     sleep 5
 done
