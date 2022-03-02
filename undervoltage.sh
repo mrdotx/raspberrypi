98db783e11afe295338195c8e3b3e34e3130d995
mrdotx <klassiker@gmx.de>
2022-03-02 08:55:22 +0100

    minor optimizations

--- undervoltage.sh
+++ undervoltage.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/raspberrypi/undervoltage.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/raspberrypi
-# date:   2021-12-17T18:49:31+0100
+# date:   2022-03-02T08:54:12+0100
 
 # speed up script by not using unicode
 LC_ALL=C
@@ -12,6 +12,9 @@ LANG=C
 # config
 vcgencmd="/opt/vc/bin/vcgencmd"
 cpu_freq="/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq"
+header="time     temp    cpu fake/real  health state        vcore"
+header_repeat=10
+measuring_interval=5
 
 # information about results
 # 0: under-voltage
@@ -34,35 +37,48 @@ cpu_freq="/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq"
 # health state        vcore
 # 0000000000000000000 1.3125V
 
-i=9
-header="time     temp    cpu fake/real  health state        vcore"
+get_throttled() {
+    $vcgencmd get_throttled \
+        | cut -f2 -dx
+}
+
+get_health() {
+    printf "obase=2; ibase=16; %s\n" "$(get_throttled)" \
+        | bc
+}
+
+get_temp() {
+    $vcgencmd measure_temp \
+        | cut -f2 -d=
+}
+
+get_real_cs() {
+    $vcgencmd measure_clock arm \
+        | awk -F"=" '{printf ("%0.0f",$2/1000000); }'
+}
+
+get_sys_cs() {
+    awk '{printf ("%0.0f",$1/1000); }' <$cpu_freq
+}
+
+get_v_core() {
+    $vcgencmd measure_volts \
+        | cut -f2 -d= \
+        | sed 's/000//'
+}
+
+i=$((header_repeat - 1))
 while true; do
-    i=$(( i + 1 ))
-    [ "$i" -eq 10 ] \
+    i=$((i + 1))
+    [ "$i" -eq $header_repeat ] \
         && printf "\n%s\n" "$header" \
         && i=0
-    throttled=$($vcgencmd get_throttled \
-        | cut -f2 -dx \
-    )
-    health=$(printf "obase=2; ibase=16; %s\n" "$throttled" \
-        | bc \
-    )
-    temp=$($vcgencmd measure_temp \
-        | cut -f2 -d= \
-    )
-    real_cs=$($vcgencmd measure_clock arm \
-        | awk -F"=" '{printf ("%0.0f",$2/1000000); }' \
-    )
-    sys_cs=$(awk '{printf ("%0.0f",$1/1000); }' <$cpu_freq)
-    v_core=$($vcgencmd measure_volts \
-        | cut -f2 -d= \
-        | sed 's/000//' \
-    )
-    printf "%s %s%5s/%4s MHz   %019d %s\n" "$(date "+%H:%M:%S")" \
-        "$temp" \
-        "$sys_cs" \
-        "$real_cs" \
-        "$health" \
-        "$v_core"
-    sleep 5
+    printf "%s %s %5s/%4s MHz  %019d %s\n" \
+        "$(date "+%H:%M:%S")" \
+        "$(get_temp)" \
+        "$(get_sys_cs)" \
+        "$(get_real_cs)" \
+        "$(get_health)" \
+        "$(get_v_core)"
+    sleep $measuring_interval
 done
