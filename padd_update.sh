029e9a4cbba4e545728abbdcbadf5793a609818b
mrdotx <klassiker@gmx.de>
2022-04-26 19:21:59 +0200

    replace ftl database location in script added

--- padd_update.sh
+++ padd_update.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/raspberrypi/padd_update.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/raspberrypi
-# date:   2022-04-24T08:01:38+0200
+# date:   2022-04-26T19:20:27+0200
 
 # speed up script by using standard c
 LC_ALL=C
@@ -12,5 +12,11 @@ LANG=C
 output="$HOME/bin/padd.sh"
 url="https://raw.githubusercontent.com/pi-hole/PADD/master/padd.sh"
 
+# download and set permissions
 curl -o "$output" "$url"
 chmod 755 "$output"
+
+# replace ftl database loaction
+search="\/run\/pihole-FTL.port"
+replace="\/run\/pihole-ftl\/pihole-FTL.port"
+sed -i "s/$search/$replace/g" "$output"
