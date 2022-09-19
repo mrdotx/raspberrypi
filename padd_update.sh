5b0300a343bac6bee6bb1cf366afd295f28a478b
mrdotx <klassiker@gmx.de>
2022-09-19 18:31:12 +0200

    replace shebang added

--- padd_update.sh
+++ padd_update.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/raspberrypi/padd_update.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/raspberrypi
-# date:   2022-04-26T19:20:27+0200
+# date:   2022-09-19T18:28:04+0200
 
 # speed up script by using standard c
 LC_ALL=C
@@ -12,11 +12,16 @@ LANG=C
 output="$HOME/bin/padd.sh"
 url="https://raw.githubusercontent.com/pi-hole/PADD/master/padd.sh"
 
+replace() {
+    sed -i "s?$1?$2?" "$output"
+}
+
 # download and set permissions
 curl -o "$output" "$url"
 chmod 755 "$output"
 
-# replace ftl database loaction
-search="\/run\/pihole-FTL.port"
-replace="\/run\/pihole-ftl\/pihole-FTL.port"
-sed -i "s/$search/$replace/g" "$output"
+# replace shebang (\e[0K problem)
+replace "#!/usr/bin/env sh" "#!/usr/bin/env bash"
+
+# replace ftl database location
+replace "/run/pihole-FTL.port" "/run/pihole-ftl/pihole-FTL.port"
